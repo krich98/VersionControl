b7a6701a4c3a665e2a57054c5a892a55a692eb45
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-10-25 15:22:36 +0100

    adatok betöltése

---
+++ Week07/Week07/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week07.Entities;
+
+namespace Week07
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool  IsAlive { get; set; }
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
--- Week07/Week07/Form1.cs
+++ Week07/Week07/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week07.Entities;
 
 namespace Week07
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
             InitializeComponent();
+            
         }
+        public void GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+        }
+
     }
 }
