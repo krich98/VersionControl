485714d2055f883de37c9eaf20f208e5eb5bc3de
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-11-15 20:57:29 +0100

    car class

---
+++ week08/week08/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using week08.Abstractions;
+
+namespace week08.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image image = Image.FromFile("Image/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
