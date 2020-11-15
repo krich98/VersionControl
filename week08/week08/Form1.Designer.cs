b2a1be2b63fd72894b1cd8bcbb7e1d7d963e7e74
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-11-15 22:00:24 +0100

    11

--- week08/week08/Form1.Designer.cs
+++ week08/week08/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblNext = new System.Windows.Forms.Label();
+            this.Carbtn = new System.Windows.Forms.Button();
+            this.ballbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -53,15 +56,48 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(401, 148);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(35, 13);
+            this.lblNext.TabIndex = 1;
+            this.lblNext.Text = "label1";
+            // 
+            // Carbtn
+            // 
+            this.Carbtn.Location = new System.Drawing.Point(106, 47);
+            this.Carbtn.Name = "Carbtn";
+            this.Carbtn.Size = new System.Drawing.Size(75, 23);
+            this.Carbtn.TabIndex = 2;
+            this.Carbtn.Text = "Car";
+            this.Carbtn.UseVisualStyleBackColor = true;
+            this.Carbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ballbtn
+            // 
+            this.ballbtn.Location = new System.Drawing.Point(187, 47);
+            this.ballbtn.Name = "ballbtn";
+            this.ballbtn.Size = new System.Drawing.Size(75, 23);
+            this.ballbtn.TabIndex = 3;
+            this.ballbtn.Text = "Ball";
+            this.ballbtn.UseVisualStyleBackColor = true;
+            this.ballbtn.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ballbtn);
+            this.Controls.Add(this.Carbtn);
+            this.Controls.Add(this.lblNext);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +106,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button Carbtn;
+        private System.Windows.Forms.Button ballbtn;
     }
 }
 
--- week08/week08/Form1.cs
+++ week08/week08/Form1.cs
@@ -16,11 +16,16 @@ namespace week08
     {
         private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        BallFactory Factory
+        private Toy _nextToy;
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -54,5 +59,26 @@ namespace week08
                 _toys.Remove(oldestBall);
             }
         }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+        }
     }
+   
 }
