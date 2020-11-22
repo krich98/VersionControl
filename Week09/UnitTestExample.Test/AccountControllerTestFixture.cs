2f760562cbabda5846a15a2cbb4e2e1863ac688f
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-11-22 20:31:33 +0100

    tezt függvény

--- Week09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Week09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // ...
+        }
+
     }
+
+    
 }
