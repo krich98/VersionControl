46fb4983224f312040de10f50f70e541c9a8b671
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-11-29 19:28:25 +0100

    winner brain

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -19,6 +19,7 @@ namespace week10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -61,6 +62,16 @@ namespace week10
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
         }
     }
 }
